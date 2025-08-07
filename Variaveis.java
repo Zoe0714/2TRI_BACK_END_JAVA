1882ac79b49243235de405ea7762fc89de4f54f5
Zoe0714 <159541965+Zoe0714@users.noreply.github.com>
2025-08-07 09:51:42 -0300

    Add files via upload

---
+++ Variaveis.java
@@ -0,0 +1,16 @@
+public class Variaveis {
+    public static void main(String[] args){
+
+    // tipo nome = conteudo;
+
+    String meuNome = "Ana ";
+    meuNome += "Banana";
+    int idade = 20;
+    double altura = 1.72;
+    char sexo = 'F';
+    boolean casada = true;
+
+        System.out.println("Meu nome é "+meuNome+", minha idade é "+idade+" anos e tenho "+altura+" de altura. Além disso, meu sexo é "+sexo+" e sou "+casada+".");
+
+    }
+}
